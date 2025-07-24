5fd1938d593bad11c235d893189011dc17ebb814
Psilocine <psilolau@foxmail.com>
2025-07-24 10:34:29 +0800

    fix: elp configures zh-cn globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
-app.use(ElementPlus)
+app.use(ElementPlus, {
+  locale: zhCn
+})
+
 app.mount('#app') 
